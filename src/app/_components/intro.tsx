c9ffb25b802095d4bbe23fa69a8101a833a7f163
Hong-yiii <hongyilin.mail@gmail.com>
2024-12-13 23:14:23 +0900

    Changed the header in intro.tsx for

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -4,17 +4,24 @@ export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        I Build Things.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
+        Let's chat! Drop me a text!{" "}
         <a
-          href="https://nextjs.org/"
+          href="mailto:hongyilin.mail@gmail.com"
           className="underline hover:text-blue-600 duration-200 transition-colors"
         >
-          Next.js
+        <div></div>
+          Email
         </a>{" "}
-        and {CMS_NAME}.
+        <div></div>
+        <a
+          href="https://www.linkedin.com/in/hong-yi-lin-793b8b23b/"
+          className="underline hover:text-blue-600 duration-200 transition-colors"
+        >
+          LinkedIn
+        </a>{" "}
       </h4>
     </section>
   );
