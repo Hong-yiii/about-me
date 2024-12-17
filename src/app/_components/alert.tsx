bdf493a82d7a0fef5bc27c04449c85db8d69d4d6
Hong-yiii <hongyilin.mail@gmail.com>
2024-12-17 23:17:37 +0900

    fixed build errors

--- src/app/_components/alert.tsx
+++ src/app/_components/alert.tsx
@@ -7,6 +7,15 @@ type Props = {
 };
 
 const Alert = ({ preview }: Props) => {
+  return (
+    <div
+      className={cn("p-4 text-center", {
+        "bg-yellow-200 text-yellow-800": preview,
+        "bg-green-200 text-green-800": !preview,
+      })}
+    >
+      {preview ? "Preview mode is on." : "Live mode is active."}
+    </div>
+  );
 };
-
 export default Alert;
