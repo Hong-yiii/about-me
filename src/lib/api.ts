e8c65b8e9dc898d6199ab3eb758433047f84adff
Hong-yiii <hongyilin.mail@gmail.com>
2024-12-29 15:19:17 +0900

    Added Manual Ordering of Pages

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -6,6 +6,7 @@ import { join } from "path";
 // Define directories for posts and main introduction
 const postsDirectory = join(process.cwd(), "_posts");
 const mainIntroductionDirectory = join(process.cwd(), "_mainIntroduction");
+const orderFilePath = join(process.cwd(), "postOrder.json");
 
 // Get slugs (filenames) from _posts directory
 export function getPostSlugs(): string[] {
@@ -13,14 +14,15 @@ export function getPostSlugs(): string[] {
     .readdirSync(postsDirectory)
     .filter((file) => file.endsWith(".md")); // Only include Markdown files
 }
+
 // Get a single post by slug
-export function getPostBySlug(slug: string) {
+export function getPostBySlug(slug: string): Post {
   const realSlug = slug.replace(/\.md$/, "");
-  
+
   // Define possible file paths
   const postPath = join(postsDirectory, `${realSlug}.md`);
   const mainIntroPath = join(mainIntroductionDirectory, `${realSlug}.md`);
-  
+
   let fullPath = "";
 
   // Check which path exists
@@ -42,10 +44,33 @@ export function getPostBySlug(slug: string) {
 // Get all posts from the _posts directory
 export function getAllPosts(): Post[] {
   const slugs = getPostSlugs();
-  const posts = slugs
-    .map((slug) => getPostBySlug(slug))
-    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1)); // Sort posts by date (newest first)
-  return posts;
+  const posts = slugs.map((slug) => getPostBySlug(slug));
+
+  let order: string[] = [];
+  try {
+    // Read the order JSON file
+    const orderFile = fs.readFileSync(orderFilePath, "utf8");
+    order = JSON.parse(orderFile).order;
+  } catch (error) {
+    console.warn(
+      `Error reading or parsing postOrder.json: ${error.message}. Defaulting to no specific order.`
+    );
+  }
+
+  // Sort posts based on the custom order if it exists
+  const orderedPosts = posts.sort((a, b) => {
+    const indexA = order.indexOf(a.slug);
+    const indexB = order.indexOf(b.slug);
+
+    // If a post is not found in the order list, place it at the end
+    if (indexA === -1 && indexB === -1) return 0; // Both missing, keep existing order
+    if (indexA === -1) return 1; // A missing, B takes precedence
+    if (indexB === -1) return -1; // B missing, A takes precedence
+
+    return indexA - indexB;
+  });
+
+  return orderedPosts;
 }
 
 // Function to retrieve the main introduction post
